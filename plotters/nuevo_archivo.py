2b231a08f6fe53035dabb400a5e637bf8cc41753
celessana <89281901+celessana@users.noreply.github.com>
2024-04-27 20:11:42 -0300

    Update nuevo_archivo.py

--- plotters/nuevo_archivo.py
+++ plotters/nuevo_archivo.py
@@ -1,23 +1,6 @@
 import seaborn.objects as so
 from gapminder import gapminder
 
-
-def plot():
-    figura = (
-        so.Plot(gapminder[["continent", "country"]].drop_duplicates(), x="continent")
-        .add(so.Bar(), so.Hist())
-        .label(
-            title="Cantidad de países por continente",
-            x="Continente",
-            y="Cantidad de países",
-        )
-    )
-    return dict(
-        descripcion="Un sofisticado gráfico con el número de países en cada continente",
-        autor="La cátedra",
-        figura=figura,
-    )
-    
 def plot():
     figura = (
         so.Plot(
